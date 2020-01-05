25a78b43bb7afb54cd7f9c7ec579bba1963fd057
tesujiro <tesujiro@gmail.com>
2020-01-05 10:41:49 +0900

    add command arg

--- 2ndEdition/ch15/ex4.hs
+++ 2ndEdition/ch15/ex4.hs
@@ -1,5 +1,9 @@
+import           System.Environment (getArgs)
+
 fibs :: [Integer]
 fibs = 0:1:[x+y | (x,y) <- zip fibs (tail fibs)]
 
 main = do
-     print $ take 50 fibs
+     args <- getArgs
+     let n = if length args > 0 then read (args !! 0)::Int else 50
+     print $ take n fibs
