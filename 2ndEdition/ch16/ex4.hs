133646ae39cbc3e116b26c1d831c0c07e9d9b04d
tesujiro <tesujiro@gmail.com>
2020-01-08 22:24:19 +0900

    add

---
+++ 2ndEdition/ch16/ex4.hs
@@ -0,0 +1,51 @@
+-- Definition
+[] ++ ys = ys
+(x:xs) ++ ys = x : (xs ++ ys)
+
+-- Propotion 1
+xs ++ [] = xs
+
+-- Base
+[] ++ []
+ -- Definition
+ = []
+
+-- Recursion
+if:
+xs ++ [] = xs
+
+then:
+x:xs ++ []
+ -- Definition
+ = x : (xs ++ [])
+ -- Assumption
+ = x : xs
+
+
+-- Propotion 2
+xs ++ (ys ++ zs) = (xs ++ ys) ++ zs
+
+-- Base
+[] ++ (ys ++ zs)
+ -- Definition
+ = ys ++ zs
+ -- Definition
+ = ([] ++ ys) ++ zs
+
+-- Recusion
+if:
+xs ++ (ys ++ zs) = (xs ++ ys) ++ zs
+
+then:
+(x:xs) ++ (ys ++ zs)
+ -- Definition
+ = x : (xs ++ (ys ++ zs))
+ -- Assumption
+ = x : ((xs ++ ys) ++zs)
+ -- reverse Definition
+ = (x:(xs ++ ys)) ++zs
+ -- reverse Definition
+ = ((x:xs) ++ ys) ++ zs
+
+ 
+ 
